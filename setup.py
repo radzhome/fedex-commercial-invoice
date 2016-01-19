e1475c1e87da4708656d01b2edd6802cb230a9d9
Rad <radzhome@gmail.com>
2016-01-19 01:40:05 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,31 @@
 from setuptools import setup, find_packages
 
 
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+]
+
+KEYWORDS = 'fedex commercial invoice generator using reportlab'
+
 setup(
-    name='fedex_commercial_invoice',
+    name='FedexInvoice',
     version='0.1',
+    author='Radzhome',
     author_email='radzhome@gmail.com',
     packages=find_packages(),
     install_requires=['reportlab'],
     include_package_data=True,
     url='https://github.com/radlws/fedex-commercial-invoice/',
-    license='',
+    license='BSD',
     description='Fedex Commercial Invoice generator.',
     long_description=open('README.md').read(),
-    zip_safe=False,
+    platforms=['Platform Independent'],
+    classifiers=CLASSIFIERS,
+    keywords=KEYWORDS,
 )
